a0628bcdb351f1154c48f8edfc482a7f1e976c5a
Selzer <nselzer@amazon.com>
2018-02-26 10:08:54 -0500

    Finished 2-9

--- src/set2/problem9/problem2_9.py
+++ src/set2/problem9/problem2_9.py
@@ -3,12 +3,16 @@ def main():
     unpadded = raw_input()
     print("Enter block size")
     block_size = input()
-    print(pad(unpadded, block_size))
+    # Print as byte-array to see non-ascii characters
+    print(repr(pad(unpadded, block_size)))
+
 
 
 def pad(unpadded, block_size):
     pad_length = block_size - len(unpadded) % block_size
-    padded_string = unpadded + bytes(pad_length)*pad_length
+    if pad_length == 0:
+        pad_length = block_size
+    padded_string = bytes(unpadded + bytearray([pad_length]*pad_length))
     return padded_string
 
 
